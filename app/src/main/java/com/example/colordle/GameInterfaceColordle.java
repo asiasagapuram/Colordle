8570cfa9eb1644c87c628aaed26c7dee47682aa2
Jason Dinh <47726701+jDinhBC@users.noreply.github.com>
2022-04-24 03:52:22 -0400

    Implemented Game Logic

---
+++ app/src/main/java/com/example/colordle/GameInterfaceColordle.java
@@ -0,0 +1,7 @@
+package com.example.colordle;
+
+public interface GameInterfaceColordle {
+    void checkData();
+    int[] checkGuess(String [] currGuess);
+    boolean checkGuess2(String hexColor);
+}
