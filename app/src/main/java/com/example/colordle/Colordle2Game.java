ef50bd4065e153749cf0db1519e7d57fd048498c
Jason Dinh <47726701+jDinhBC@users.noreply.github.com>
2022-05-03 11:25:20 -0400

    Colordle game implementation (#12)
    * Updated class for data storage
    
    started on implementation of gameplay
    
    * Basic Ui for Colordle 1
    
    * Started Implementation of Colordle
    
    skeleton methods for game.
    
    * Implemented autoswapping focus
    
    * Implemented editTexts to autoswitch
    
    convenience feature
    
    * bug fixes and implemented flip animation
    
    * UI fixes and improvements
    
    * Completed Colordle1 and Animations
    
    * Fixed Lag from Mediaplayer
    
    * Update twinkle.mp3
    
    * Added Delay for popup for animation
    
    * Update sky.png
    
    saves memory

--- app/src/main/java/com/example/colordle/Colordle2Game.java
+++ app/src/main/java/com/example/colordle/Colordle2Game.java
@@ -11,6 +11,7 @@ public class Colordle2Game extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
         setContentView(R.layout.activity_colordle2_game);
     }
 
